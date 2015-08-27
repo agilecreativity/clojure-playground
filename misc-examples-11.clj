da4b647976e7196721270bc169a4d5af07d1c96c
Burin Choomnuan <agilecreativity@gmail.com>
2015-08-28 08:29:31 +1000

    Add more examples

--- misc-examples-11.clj
+++ misc-examples-11.clj
@@ -40,3 +40,30 @@
        display-str))
 ;; =>
 ;; ("animal-mouse-color-red" "animal-mouse-color-blue" "animal-duck-color-red" "animal-duck-color-blue" "animal-lory-color-red" "animal-lory-color-blue")
+
+(prn (for [animal [:mouse :duck :lory]
+           color [:red :blue]
+           :let [animal-str (str "animal-" (name animal))
+                 color-str (str "color-" (name color))
+                 display-str (str animal-str "-" color-str)]
+           :when (= color :blue)]
+       display-str))
+;; => ("animal-mouse-color-blue" "animal-duck-color-blue" "animal-lory-color-blue")
+
+(prn (flatten [[:duck [:mouse] [[:lory]]]]))
+;; => (:duck :mouse :lory)
+
+(prn (vec '(1 2 3)))
+;; => [1 2 3]
+
+(prn (into [] '(1 2 3)))
+;; => [1 2 3]
+
+(prn (sorted-map :b 2 :a 1 :z 3))
+;; => {:a 1, :b 2, :z 3}
+
+(prn (into (sorted-map) {:b 2 :c 3 :a 1}))
+;; => {:a 1, :b 2, :c 3}
+
+(prn (into {} [[:a 1] [:b 2] [:c 3]]))
+;; => {:a 1, :b 2, :c 3}

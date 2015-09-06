5062bbad093b62adc3cae842cb863a0a7a56b9f5
Burin Choomnuan <agilecreativity@gmail.com>
2015-09-07 08:23:13 +1000

    Add more examples

--- misc-examples-22.clj
+++ misc-examples-22.clj
@@ -5,7 +5,16 @@
             :blue 3
             :green 1})
 
-(println "Hello World")
-;; (reduce + (vals (select-keys beans [:red :green])))
+(prn (reduce + (vals (select-keys beans [:red :green]))))
+;; => 11
 
-(+ 2 3 4 8)
+(prn ((juxt :red :green) beans))
+;; => [10 1]
+
+(def villain {:horrific "Dr." :name "Mayhem"})
+(prn (assoc villain :occupuation "Mad Scientis" :status :at-large))
+;; => {:horrific "Dr.", :name "Mayhem", :occupuation "Mad Scientis", :status :at-large}
+
+(def villain {:horrific "Dr." :name "Mayhem", :status :deceased})
+(prn (dissoc villain :occupation :honorific))
+;; => {:horrific "Dr.", :name "Mayhem", :status :deceased}
